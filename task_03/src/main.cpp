16772cd1385354ac5ee3dc8657eec1337efe700d
Pavel-bur-dot <burachekpavel@gmail.com>
2024-11-27 08:28:40 +0000

    Reverse_funk

--- task_03/src/main.cpp
+++ task_03/src/main.cpp
@@ -1,1 +1,24 @@
-int main() { return 0; }
+#include "reverse.hpp"
+#include <iostream>
+
+int main() 
+try
+{
+    setlocale(LC_ALL, "RU");
+
+    int array[4]{ 1, 2, 3, 4 };
+    int len = 4;
+    
+    Reverse(array, len);
+
+    std::cout << "Инвертированный массив:" << std::endl;
+
+    for (int i{ 0 }; i < len; i++)
+        std::cout << array[i] << " ";
+	
+    return 0;
+}
+catch(const std::exception& e)
+{
+    std::cerr << e.what() << '\n';
+}
--- task_03/src/reverse.hpp
+++ task_03/src/reverse.hpp
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <cstddef>
 
-void Reverse(int *array, size_t len);
+void Reverse(int* array, size_t len);
